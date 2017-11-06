c18b87ede8d8082887dbe0cf986b1dbb1e6a4a20
Jonas Schnelli <dev@jonasschnelli.ch>
2017-11-05 16:34:40 -1000

    Update ban-state in case of dirty-state during periodic sweep

--- src/net.cpp
+++ src/net.cpp
@@ -593,6 +593,11 @@ void CConnman::SweepBanned()
         else
             ++it;
     }
+
+    // update UI
+    if(setBannedIsDirty && clientInterface) {
+        clientInterface->BannedListChanged();
+    }
 }
 
 bool CConnman::BannedSetIsDirty()
