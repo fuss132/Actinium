222803c4957b698596829bf037571f1cc45acce3
Ben Woosley <ben.woosley@gmail.com>
2018-02-16 12:57:19 -0800

    rpc: Drop redundant testing of signrawtransaction prevtxs args
    These other types are already tested on line 736.

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -837,9 +837,6 @@ UniValue signrawtransaction(const JSONRPCRequest& request)
             if (fGivenKeys && (scriptPubKey.IsPayToScriptHash() || scriptPubKey.IsPayToWitnessScriptHash())) {
                 RPCTypeCheckObj(prevOut,
                     {
-                        {"txid", UniValueType(UniValue::VSTR)},
-                        {"vout", UniValueType(UniValue::VNUM)},
-                        {"scriptPubKey", UniValueType(UniValue::VSTR)},
                         {"redeemScript", UniValueType(UniValue::VSTR)},
                     });
                 UniValue v = find_value(prevOut, "redeemScript");
