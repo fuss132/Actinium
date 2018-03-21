eb5690e979d7338066a9685a970882168329932d
Gregory Sanders <gsanders87@gmail.com>
2018-03-21 15:29:23 -0400

    feebumper: discard change outputs below discard rate

--- src/wallet/feebumper.cpp
+++ src/wallet/feebumper.cpp
@@ -220,7 +220,7 @@ CFeeBumper::CFeeBumper(const CWallet *pWallet, const uint256 txidIn, const CCoin
 
     // If the output would become dust, discard it (converting the dust to fee)
     poutput->nValue -= nDelta;
-    if (poutput->nValue <= GetDustThreshold(*poutput, ::dustRelayFee)) {
+    if (poutput->nValue <= GetDustThreshold(*poutput, GetDiscardRate(::feeEstimator))) {
         LogPrint(BCLog::RPC, "Bumping fee and discarding dust output\n");
         nNewFee += poutput->nValue;
         mtx.vout.erase(mtx.vout.begin() + nOutput);
