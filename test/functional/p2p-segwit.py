c256579c3dab92f5040fecb65c4d8e0fd57775ce
MarcoFalke <falke.marco@gmail.com>
2018-03-26 17:24:17 -0400

    doc: Refer to witness reserved value as spec. in the BIP

--- test/functional/p2p-segwit.py
+++ test/functional/p2p-segwit.py
@@ -419,7 +419,7 @@ class SegWitTest(BitcoinTestFramework):
 
         assert(self.nodes[0].getbestblockhash() == block.hash)
 
-        # Now make sure that malleating the witness nonce doesn't
+        # Now make sure that malleating the witness reserved value doesn't
         # result in a block permanently marked bad.
         block = self.build_next_block()
         add_witness_commitment(block)
@@ -430,7 +430,7 @@ class SegWitTest(BitcoinTestFramework):
         block.vtx[0].wit.vtxinwit[0].scriptWitness.stack = [ ser_uint256(1) ]
         self.test_node.test_witness_block(block, accepted=False)
 
-        # Changing the witness nonce doesn't change the block hash
+        # Changing the witness reserved value doesn't change the block hash
         block.vtx[0].wit.vtxinwit[0].scriptWitness.stack = [ ser_uint256(0) ]
         self.test_node.test_witness_block(block, accepted=True)
 
