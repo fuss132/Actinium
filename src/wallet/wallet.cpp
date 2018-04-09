f76de6c4cf8773078159f0fc39dad339d7e1e136
Jonas Schnelli <dev@jonasschnelli.ch>
2018-04-09 18:48:19 +0200

    Logprint the start of a rescan

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1667,6 +1667,9 @@ CBlockIndex* CWallet::ScanForWalletTransactions(CBlockIndex* pindexStart, CBlock
 
     CBlockIndex* pindex = pindexStart;
     CBlockIndex* ret = nullptr;
+
+    if (pindex) LogPrintf("Rescan started from block %d...\n", pindex->nHeight);
+
     {
         LOCK2(cs_main, cs_wallet);
         fAbortRescan = false;
